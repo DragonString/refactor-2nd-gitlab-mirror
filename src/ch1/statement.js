f84cacf8d4d9c8f0707b47e79b67419dc817f880
AWOS <uws0331@gmail.com>
2022-12-27 20:19:17 +0900

    refactor: move amountFor method in statement method

--- src/ch1/statement.js
+++ src/ch1/statement.js
@@ -29,36 +29,35 @@ function statement(invoice, plays) {
 
     return result;
 
-    function playFor(aPerformance){
+    function amountFor(aPerformance, play) {
+        let result = 0;
+
+        switch (play.type) {
+            case "tragedy": // 비극
+                result = 40000;
+                if (aPerformance.audience > 30) {
+                    result += 1000 * (aPerformance.audience - 30);
+                }
+                break;
+
+            case "comedy": // 희극
+                result = 30000;
+                if (aPerformance.audience > 20) {
+                    result += 10000 + 500 * (aPerformance.audience - 20);
+                }
+                result += 300 * aPerformance.audience;
+                break;
+
+            default:
+                throw new Error(`알 수 없는 장르: ${play.type}`);
+        }
+        return result;
+    }
+
+    function playFor(aPerformance) {
         return plays[aPerformance.playID];
     }
 }
-
-function amountFor(aPerformance, play) {
-    let result = 0;
-
-    switch (play.type) {
-        case "tragedy": // 비극
-            result = 40000;
-            if (aPerformance.audience > 30) {
-                result += 1000 * (aPerformance.audience - 30);
-            }
-            break;
-
-        case "comedy": // 희극
-            result = 30000;
-            if (aPerformance.audience > 20) {
-                result += 10000 + 500 * (aPerformance.audience - 20);
-            }
-            result += 300 * aPerformance.audience;
-            break;
-
-        default:
-            throw new Error(`알 수 없는 장르: ${play.type}`);
-    }
-    return result;
-}
-
 
 
 // NOTE: 예제 코드 실행을 위한 임시 구문
