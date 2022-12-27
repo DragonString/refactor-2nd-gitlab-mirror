d183fcfb9c3d407b2c8fa9e435136e19e7ac018c
AWOS <uws0331@gmail.com>
2022-12-27 21:15:14 +0900

    refactor: remove totalAmount variable (3. extract totalAmount variable to appleSauce method)

--- src/ch1/statement.js
+++ src/ch1/statement.js
@@ -9,10 +9,7 @@ function statement(invoice, plays) {
         // 청구 내역을 출력한다.
         result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`
     }
-    let totalAmount = 0;
-    for (let perf of invoice.performances) {
-        totalAmount += amountFor(perf);
-    }
+    let totalAmount = appleSauce();
     result += `총액: ${usd(totalAmount / 100)}\n`;
     result += `적립 포인트: ${totalVolumeCredits()}점\n`;
 
@@ -66,6 +63,14 @@ function statement(invoice, plays) {
             volumeCredits += volumeCreditsFor(perf);
         }
         return volumeCredits;
+    }
+
+    function appleSauce() {
+        let totalAmount = 0;
+        for (let perf of invoice.performances) {
+            totalAmount += amountFor(perf);
+        }
+        return totalAmount;
     }
 }
 
