6f0d2c666a7c40073cd1622998d57c8281d43c99
AWOS <uws0331@gmail.com>
2022-12-27 22:08:37 +0900

    fix: correction due to incorrect calculation

--- src/ch1/statement.js
+++ src/ch1/statement.js
@@ -14,7 +14,7 @@ function renderPlainText(invoice, plays) {
         // 청구 내역을 출력한다.
         result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`
     }
-    result += `총액: ${usd(totalAmount() / 100)}\n`;
+    result += `총액: ${usd(totalAmount())}\n`;
     result += `적립 포인트: ${totalVolumeCredits()}점\n`;
 
     return result;
