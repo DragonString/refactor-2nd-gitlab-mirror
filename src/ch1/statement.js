d5c1eefc3364b3d05af37ba49c49e4b50d175727
AWOS <uws0331@gmail.com>
2022-12-27 20:52:01 +0900

    refactor: remove volumeCredits variable (2. slide volumeCredits variable)

--- src/ch1/statement.js
+++ src/ch1/statement.js
@@ -4,7 +4,6 @@ const require = createRequire(import.meta.url);
 
 function statement(invoice, plays) {
     let totalAmount = 0;
-    let volumeCredits = 0;
     let result = `청구 내역 (고객명: ${invoice.customer})\n`;
 
     for (let perf of invoice.performances) {
@@ -13,6 +12,7 @@ function statement(invoice, plays) {
         result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`
         totalAmount += amountFor(perf);
     }
+    let volumeCredits = 0;
     for (let perf of invoice.performances) {
         volumeCredits += volumeCreditsFor(perf);
     }
