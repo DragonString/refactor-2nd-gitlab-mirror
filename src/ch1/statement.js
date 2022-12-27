64e27c4749b35fb498e1600d59bb4c82551d7fcf
AWOS <uws0331@gmail.com>
2022-12-27 21:30:35 +0900

    refactor: separate calculation Step from formatting Step(extract renderPlainText method and statement method)

--- src/ch1/statement.js
+++ src/ch1/statement.js
@@ -3,6 +3,11 @@ import {createRequire} from "module";
 const require = createRequire(import.meta.url);
 
 function statement(invoice, plays) {
+    return renderPlainText(invoice, plays)
+
+}
+
+function renderPlainText(invoice, plays) {
     let result = `청구 내역 (고객명: ${invoice.customer})\n`;
 
     for (let perf of invoice.performances) {
