0ed423935b9483ee1c81ed554076489123ed10ca
AWOS <uws0331@gmail.com>
2022-12-27 20:36:11 +0900

    refactor: 임시 변수 였던 format 을 함수 호출로 대체

--- src/ch1/statement.js
+++ src/ch1/statement.js
@@ -6,10 +6,6 @@ function statement(invoice, plays) {
     let totalAmount = 0;
     let volumeCredits = 0;
     let result = `청구 내역 (고객명: ${invoice.customer})\n`;
-    const format = new Intl.NumberFormat(
-        "en-US",
-        {style: "currency", currency: "USD", minimumFractionDigits: 2}
-    ).format;
 
     for (let perf of invoice.performances) {
         volumeCredits += volumeCreditsFor(perf);
@@ -58,6 +54,12 @@ function statement(invoice, plays) {
         result += Math.max(aPerformance.audience - 30, 0);
         if ("comedy" === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5);
         return result
+    }
+
+    function format(aNumber) {
+        return new Intl.NumberFormat(
+            "en-US",
+            {style: "currency", currency: "USD", minimumFractionDigits: 2}).format(aNumber);
     }
 }
 
