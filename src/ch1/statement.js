bb6868631fb955a42e25ec8c730d9fea64ece0b0
AWOS <uws0331@gmail.com>
2022-12-28 18:21:54 +0900

    refactor: "statement() 에 필요한 데이터 처리" 에 해당하는 코드를 별도 함수로 createStatementData() 로 처리

--- src/ch1/statement.js
+++ src/ch1/statement.js
@@ -3,12 +3,16 @@ import {createRequire} from "module";
 const require = createRequire(import.meta.url);
 
 function statement(invoice, plays) {
+    return renderPlainText(createStatementData(invoice, plays));
+}
+
+function createStatementData(invoice, plays) {
     const statementData = {};
     statementData.customer = invoice.customer;
     statementData.performances = invoice.performances.map(enrichPerformance);
     statementData.totalAmount = totalAmount(statementData);
     statementData.totalVolumeCredits = totalVolumeCredits(statementData);
-    return renderPlainText(statementData, plays)
+    return statementData;
 
     function enrichPerformance(aPerformance) {
         const result = Object.assign({}, aPerformance); // 얕은 복사 수행
