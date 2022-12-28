1d9c66cf836ab250ae0c48725b17adbf75ebd0ef
AWOS <uws0331@gmail.com>
2022-12-28 18:40:15 +0900

    refactor: 1.7 중간 점검: 두파일(과 두 단계)로 분리

--- src/ch1/createStatementData.js
+++ src/ch1/createStatementData.js
@@ -12,7 +12,7 @@ export default function createStatementData(invoice, plays) {
         result.amount = amountFor(result);
         result.volumeCredits = volumeCreditsFor(result);
 
-        return result
+        return result;
     }
 
     function playFor(aPerformance) {
